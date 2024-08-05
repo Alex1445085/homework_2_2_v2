ab297e206113d2b6ed72a19a70d0cbc962eb855a
Alex1445085 <n1445085@yandex.ru>
2024-08-05 09:03:26 +0300

    no commit

---
+++ src/Griffindor.java
@@ -0,0 +1,33 @@
+public class Griffindor extends Hogvards{
+
+    private int brave;
+    private int honor;
+
+    public Griffindor(String name, int magicPower, int transGression, int brave, int honor) {
+        super(name, magicPower, transGression);
+        this.brave = brave;
+        this.honor = honor;
+    }
+
+    public int getBrave() { return this.getBrave(); }
+    public int getHonor() { return this.getHonor(); }
+    public void compareStudents(Griffindor student) {
+        if (this.brave + this.honor > student.brave + student.honor) {
+            System.out.println("Student " + this.getName() + " the best then " + student.getName());
+        } else if(this.brave + this.honor < student.brave + student.honor) {
+            System.out.println("Student " + student.getName() + " the best then " + this.getName());
+        } else {
+            System.out.println("Students " + this.getName() + " & " + student.getName() + " the same");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", brave " + brave + ", honor " + honor;
+    }
+
+    @Override
+    public void says(Hogvards obj) {
+        System.out.println(obj.getName() + ": I am a Griffindorian!");
+    }
+}
---
+++ src/Slyserin.java
@@ -0,0 +1,27 @@
+public class Slyserin extends Hogvards {
+
+    private int vlast;
+    private int ambition;
+    private int determination;
+
+    public Slyserin(String name, int magicPower, int transGression, int vlast, int ambition, int determination) {
+        super(name, magicPower, transGression);
+        this.vlast = vlast;
+        this.ambition = ambition;
+        this.determination = determination;
+    }
+
+    public int getVlast() { return vlast; }
+    public int getAmbition() { return ambition; }
+    public int getDetermination() { return this.determination; }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", vlast " + vlast + ", ambition " + ambition + ", determination " + determination;
+    }
+/**
+    @Override
+    public void says(Hogvards obj) {
+        System.out.println(obj.getName() + ": I am a Slyserinian");
+    }*/
+}
